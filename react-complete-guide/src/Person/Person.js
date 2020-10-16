aa66f00b440a9109867b5ca12ccd6e80141655f8
Abhishek <abhi.dagwar@gmail.com>
2020-10-16 00:22:18 -0300

    Adding styling with stylesheet

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Person.css'
 
 //This is a stateless/dumb/presentational component because we are not using state here.
 const person = (props) => {
   //Passing method reference between component.
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>I am {props.name} and I am {props.age} years old.</p>
       <p>{props.children}</ p>
       <input type="text" onChange={props.change} value={props.name}/>
