3516b07ed79380cf7ccb553f81b8a19c69c5c402
Abhishek <abhi.dagwar@gmail.com>
2020-10-16 00:07:24 -0300

    Added two way binding in a input type

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -25,9 +25,19 @@ class App extends Component {
     })
   }
 
-//Create a passing reference for Person file.
-//() => this.methodName(param) --- This is a inefficient way if an app is big
-//this.methodName.bind(this, param) -- Try using this whenever possible.
+nameChangedHandler = (event) => {
+  this.setState({
+    persons: [
+      { name: 'Abhishek Dagwar', age: 33 },
+      { name: event.target.value, age: 30 },
+      { name: 'Rahul Dagwar', age: 29}
+    ]
+  })
+}
+
+  //Create a passing reference for Person file.
+  //() => this.methodName(param) --- This is a inefficient way if an app is big
+  //this.methodName.bind(this, param) -- Try using this whenever possible.
   render() {
     return (
       <div className="App">
@@ -47,7 +57,8 @@ class App extends Component {
         <Person
           name = {this.state.persons[1].name}
           age = {this.state.persons[1].age}
-          click = {this.switchNameHandler.bind(this, 'Abhi Dagwar')}> My Hobbey: Cricket</ Person>
+          click = {this.switchNameHandler.bind(this, 'Abhi Dagwar')}
+          change={this.nameChangedHandler}> My Hobbey: Cricket</ Person>
         <Person
           name = {this.state.persons[2].name}
           age = {this.state.persons[2].age} />
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -7,6 +7,7 @@ const person = (props) => {
     <div>
       <p onClick={props.click}>I am {props.name} and I am {props.age} years old.</p>
       <p>{props.children}</ p>
+      <input type="text" onChange={props.change} value={props.name}/>
     </ div>
   )
 };
