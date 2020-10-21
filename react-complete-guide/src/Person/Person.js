2d4bd230e15233e448c47a72426d1d8421234dab
Abhishek <abhi.dagwar@gmail.com>
2020-10-20 22:51:51 -0300

    Added error code

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -4,6 +4,19 @@ import cssClasses from './Person.css'
 
 //This is a stateless/dumb/presentational component because we are not using state here.
 const person = (props) => {
+  /*
+    This is just a sample code to see how error can handle in js.
+    In this example ypu can see the error is thrown but its not handle properly.
+    In production if the error is thrown then we can have to handle it too so that error page will not seen on browsers
+    but instead your error message which you want to show.
+    In this example a random number will generate and if the value is greater than 0.7 then error will thrown.
+    Please toggle the 'Toggle Person' button couple of times to see the error.
+  */
+  const rnd = Math.random();
+  if (rnd > 0.7) {
+    throw new Error('Something went wrong');
+  }
+
   //Passing method reference between component.
   return (
     <div className={cssClasses.Person}>
